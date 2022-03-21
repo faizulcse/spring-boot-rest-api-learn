1b28cd1a1ef814525f3084b13ec9d3e0d0f38b70
faizul <faizulcse@gmail.com>
2022-03-22 02:46:30 +0600

    initial commit

---
+++ src/main/java/com/aapon/springbootrestapilearn/SpringBootRestApiLearnApplication.java
@@ -0,0 +1,13 @@
+package com.aapon.springbootrestapilearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApiLearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aapon/springbootrestapilearn/SpringBootRestApiLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aapon.springbootrestapilearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApiLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
