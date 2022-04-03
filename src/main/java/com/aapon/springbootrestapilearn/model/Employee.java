2e0f9488d64d273e65635c45ada92362eeecdfa2
faizul <faizulcse@gmail.com>
2022-04-03 19:35:30 +0600

    add timestamp

--- src/main/java/com/aapon/springbootrestapilearn/model/Employee.java
+++ src/main/java/com/aapon/springbootrestapilearn/model/Employee.java
@@ -3,8 +3,11 @@ package com.aapon.springbootrestapilearn.model;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Setter
 @Getter
@@ -31,4 +34,12 @@ public class Employee {
 
     @Column(name = "department")
     private String department;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Date updatedAt;
 }
