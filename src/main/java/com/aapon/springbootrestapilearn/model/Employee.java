f3986e2c432ed9b8654028a7c6b5503ce57f79cb
faizul <faizulcse@gmail.com>
2022-04-03 19:38:35 +0600

    remove column annotation as the column names are same as table column

--- src/main/java/com/aapon/springbootrestapilearn/model/Employee.java
+++ src/main/java/com/aapon/springbootrestapilearn/model/Employee.java
@@ -17,22 +17,11 @@ import java.util.Date;
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
     private Long id;
-
-    @Column(name = "name")
     private String name;
-
-    @Column(name = "age")
     private Long age;
-
-    @Column(name = "location")
     private String location;
-
-    @Column(name = "email")
     private String email;
-
-    @Column(name = "department")
     private String department;
 
     @CreationTimestamp
