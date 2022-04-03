5722c1406de931a96be6b62a72fd7b234e54600f
faizul <faizulcse@gmail.com>
2022-04-03 17:42:31 +0600

    add location column

--- sql_script/restapi_db.sql
+++ sql_script/restapi_db.sql
@@ -6,9 +6,10 @@ CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     age INT NOT NULL,
     name VARCHAR(255) NOT NULL,
+    location VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     department VARCHAR(255) NOT NULL
 );
-INSERT INTO employee (name, age, email, department) VALUES ("faizul", 26, "faizulcse@gmail.com", "it");
-INSERT INTO employee (name, age, email, department) VALUES ("appon", 27, "aaponcse@gmail.com", "math");
-INSERT INTO employee (name, age, email, department) VALUES ("islam", 28, "islam@gmail.com", "english");
+INSERT INTO employee (name, age, location, email, department) VALUES ("faizul", 26, "Dhaka", "faizulcse@gmail.com", "it");
+INSERT INTO employee (name, age, location, email, department) VALUES ("appon", 27, "Khulna", "aaponcse@gmail.com", "math");
+INSERT INTO employee (name, age, location, email, department) VALUES ("islam", 28, "Barisal", "islam@gmail.com", "english");
