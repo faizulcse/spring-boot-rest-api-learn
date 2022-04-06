71fdb8d045b7ec3a3596b5cb57f34f4b3aea8bd2
faizul <faizulcse@gmail.com>
2022-04-06 23:20:55 +0600

    Revert "one to one relationship with 2 tables design"
    This reverts commit c8b4467c31d395c9260ecd7817ee36a4deb40a55.

--- src/main/java/com/aapon/springbootrestapilearn/model/Employee.java
+++ src/main/java/com/aapon/springbootrestapilearn/model/Employee.java
@@ -1,12 +1,15 @@
 package com.aapon.springbootrestapilearn.model;
 
-import com.aapon.springbootrestapilearn.request.EmployeeRequest;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import java.util.Date;
 
 @Setter
 @Getter
@@ -21,11 +24,21 @@ public class Employee {
     @NotBlank(message = "Name should not be null")
     private String name;
 
-    @OneToOne
-    @JoinColumn(name = "department_id")
-    private Department department;
+    private Long age = 0L;
+    private String location;
 
-    public Employee(EmployeeRequest req) {
-        this.name = req.getName();
-    }
+    @Email(message = "Please enter the valid email")
+    @NotBlank(message = "Email should not be null")
+    private String email;
+
+    @NotBlank(message = "Department should not be null")
+    private String department;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Date updatedAt;
 }
