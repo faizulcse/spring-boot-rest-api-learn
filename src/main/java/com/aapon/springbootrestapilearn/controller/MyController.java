57cbdeb6a5e0ecf1051b6c41300b3d8da844cbbe
faizul-bs23 <faizulcse@gmail.com>
2023-01-06 15:17:15 +0600

    update log format

--- src/main/java/com/aapon/springbootrestapilearn/controller/MyController.java
+++ src/main/java/com/aapon/springbootrestapilearn/controller/MyController.java
@@ -2,6 +2,7 @@ package com.aapon.springbootrestapilearn.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -15,8 +16,8 @@ public class MyController {
     private static long count = 1;
 
     @GetMapping("/info")
-    public ResponseEntity<String> getAppInfo() {
-        String log = String.format("%s. [%s]=============> %s", count++, LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")), "version-v1");
+    public synchronized ResponseEntity<String> getAppInfo(@RequestHeader(value = "User-Agent") String userAgent) {
+        String log = String.format("%s. [%s] ====(%s)====> [%s]", count++, LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")), "version-v1", userAgent);
         System.out.println(log);
         return ResponseEntity.ok().body(log);
     }
