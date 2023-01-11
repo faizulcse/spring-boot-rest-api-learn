926ad170cd8c37f11fa2c2d748c7f1a8a3671abc
faizul-bs23 <faizulcse@gmail.com>
2023-01-11 13:10:13 +0600

    add new test

--- src/main/java/com/aapon/springbootrestapilearn/controller/MyController.java
+++ src/main/java/com/aapon/springbootrestapilearn/controller/MyController.java
@@ -1,5 +1,6 @@
 package com.aapon.springbootrestapilearn.controller;
 
+import com.github.javafaker.Faker;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -8,16 +9,22 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-;
-
-
 @RestController
 public class MyController {
     private static long count = 1;
 
     @GetMapping("/info")
-    public synchronized ResponseEntity<String> getAppInfo(@RequestHeader(value = "User-Agent") String userAgent) {
+    public ResponseEntity<String> getAppInfo(@RequestHeader(value = "User-Agent") String userAgent) {
         String log = String.format("%s. [%s] ====(%s)====> [%s]", count++, LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")), "version-v1", userAgent);
+        System.out.println(log);
+        return ResponseEntity.ok().body(log);
+    }
+
+    @GetMapping("/load_test")
+    public ResponseEntity<String> loadTest(@RequestHeader(value = "User-Agent") String userAgent) throws InterruptedException {
+        long wait = new Faker().random().nextInt(1, 10) * 100;
+        Thread.sleep(wait);
+        String log = String.format("%s. [%s] ====(%s)====> [%s] ===> %s", count++, LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")), "version-v1", userAgent, wait);
         System.out.println(log);
         return ResponseEntity.ok().body(log);
     }
