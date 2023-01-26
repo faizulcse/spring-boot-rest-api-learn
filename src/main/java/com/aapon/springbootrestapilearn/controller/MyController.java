de7cf7e7e1935305117be648d582426f37626d4b
faizul-bs23 <faizulcse@gmail.com>
2023-01-26 15:51:34 +0600

    add more testcases

--- src/main/java/com/aapon/springbootrestapilearn/controller/MyController.java
+++ src/main/java/com/aapon/springbootrestapilearn/controller/MyController.java
@@ -2,9 +2,7 @@ package com.aapon.springbootrestapilearn.controller;
 
 import com.github.javafaker.Faker;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -13,11 +11,60 @@ import java.time.format.DateTimeFormatter;
 public class MyController {
     private static long count = 1;
 
-    @GetMapping("/info")
-    public ResponseEntity<String> getAppInfo(@RequestHeader(value = "User-Agent") String userAgent) {
+    @GetMapping("/normalApi")
+    public ResponseEntity<String> normalApi(@RequestHeader(value = "User-Agent") String userAgent) {
         String log = String.format("%s. [%s] ====(%s)====> [%s]", count++, LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")), "version-v1", userAgent);
         System.out.println(log);
         return ResponseEntity.ok().body(log);
+    }
+
+    @GetMapping("/fixedDelayApi")
+    public ResponseEntity<String> fixedDelayApi(@RequestHeader(value = "User-Agent") String userAgent) throws InterruptedException {
+        int wait = 999;
+        Thread.sleep(wait);
+        String log = String.format("%s. [%s] ====(%s)====> [%s] ===> %s", count++, LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")), "version-v1", userAgent, wait);
+        System.out.println(log);
+        return ResponseEntity.ok().body(log);
+    }
+
+    @GetMapping("/randomDelayApi")
+    public ResponseEntity<String> randomDelayApi(@RequestHeader(value = "User-Agent") String userAgent) throws InterruptedException {
+        long wait = new Faker().random().nextInt(1, 10) * 100;
+        Thread.sleep(wait);
+        String log = String.format("%s. [%s] ====(%s)====> [%s] ===> %s", count++, LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")), "version-v1", userAgent, wait);
+        System.out.println(log);
+        return ResponseEntity.ok().body(log);
+    }
+
+    @GetMapping("/normalSyncApi")
+    public synchronized ResponseEntity<String> normalSyncApi(@RequestHeader(value = "User-Agent") String userAgent) {
+        String log = String.format("%s. [%s] ====(%s)====> [%s]", count++, LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")), "version-v1", userAgent);
+        System.out.println(log);
+        return ResponseEntity.ok().body(log);
+    }
+
+    @GetMapping("/fixedDelaySyncApi")
+    public synchronized ResponseEntity<String> fixedDelaySyncApi(@RequestHeader(value = "User-Agent") String userAgent) throws InterruptedException {
+        int wait = 999;
+        Thread.sleep(wait);
+        String log = String.format("%s. [%s] ====(%s)====> [%s] ===> %s", count++, LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")), "version-v1", userAgent, wait);
+        System.out.println(log);
+        return ResponseEntity.ok().body(log);
+    }
+
+    @GetMapping("/randomDelaySyncApi")
+    public synchronized ResponseEntity<String> randomDelaySyncApi(@RequestHeader(value = "User-Agent") String userAgent) throws InterruptedException {
+        long wait = new Faker().random().nextInt(1, 10) * 100;
+        Thread.sleep(wait);
+        String log = String.format("%s. [%s] ====(%s)====> [%s] ===> %s", count++, LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")), "version-v1", userAgent, wait);
+        System.out.println(log);
+        return ResponseEntity.ok().body(log);
+    }
+
+    @PostMapping("/printApiBody")
+    public ResponseEntity<String> printApiBody(@RequestBody String data) {
+        System.out.println(data);
+        return ResponseEntity.ok().body(data);
     }
 
     @GetMapping("/load_test")
