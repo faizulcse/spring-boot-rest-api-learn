c1df9faf4a7e18cda58ce7a2b29546015c36ca1d
faizul-bs23 <faizulcse@gmail.com>
2023-01-26 16:01:22 +0600

    add jmeter script

--- src/main/java/com/aapon/springbootrestapilearn/controller/MyController.java
+++ src/main/java/com/aapon/springbootrestapilearn/controller/MyController.java
@@ -18,6 +18,15 @@ public class MyController {
         return ResponseEntity.ok().body(log);
     }
 
+    @GetMapping("/load_test")
+    public ResponseEntity<String> loadTest(@RequestHeader(value = "User-Agent") String userAgent) throws InterruptedException {
+        long wait = new Faker().random().nextInt(1, 10) * 100;
+        Thread.sleep(wait);
+        String log = String.format("%s. [%s] ====(%s)====> [%s] ===> %s", count++, LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")), "version-v1", userAgent, wait);
+        System.out.println(log);
+        return ResponseEntity.ok().body(log);
+    }
+
     @GetMapping("/fixedDelayApi")
     public ResponseEntity<String> fixedDelayApi(@RequestHeader(value = "User-Agent") String userAgent) throws InterruptedException {
         int wait = 999;
@@ -65,14 +74,5 @@ public class MyController {
     public ResponseEntity<String> printApiBody(@RequestBody String data) {
         System.out.println(data);
         return ResponseEntity.ok().body(data);
-    }
-
-    @GetMapping("/load_test")
-    public ResponseEntity<String> loadTest(@RequestHeader(value = "User-Agent") String userAgent) throws InterruptedException {
-        long wait = new Faker().random().nextInt(1, 10) * 100;
-        Thread.sleep(wait);
-        String log = String.format("%s. [%s] ====(%s)====> [%s] ===> %s", count++, LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")), "version-v1", userAgent, wait);
-        System.out.println(log);
-        return ResponseEntity.ok().body(log);
     }
 }
