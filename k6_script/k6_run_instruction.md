20e56b3a65a5f219ece82f244bf037920eafc0a0
faizul-bs23 <faizulcse@gmail.com>
2023-01-30 01:30:46 +0600

    add k6 loadtest script

---
+++ k6_script/k6_run_instruction.md
@@ -0,0 +1,1 @@
+Run `k6 run ./k6_script/example_k6.js`
---
+++ k6_script/spring_boot_api_example_k6.js
@@ -0,0 +1,24 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export const options = {
+  discardResponseBodies: true,
+  vus: 100,
+  iterations:1000
+};
+
+export function setup() {
+    console.log("=====setUp==========> ");
+}
+
+export default function(data){
+    const res = http.get('http://localhost:9090/api/v1/normalApi');
+    console.log("=====execute_api====> ", res.status);
+    check(res, {
+        'is status 200': (r) => r.status === 200,
+      });
+}
+
+export function teardown(data) {
+    console.log("=====tearDown=======> ");
+}
