2e01f150b671403ec7433f7510a49d72c14f98f1
faizul.bs23 <faizulcse@gmail.com>
2026-01-17 23:29:56 +0600

    Added rate limit feature

--- load_test_demo/tests/script.js
+++ load_test_demo/tests/script.js
@@ -2,13 +2,13 @@ import http from 'k6/http';
 import { checkAndVerifyStatus, endPoints } from '../utils/index.js';
 
 export const options = {
-  vus: 1,
-  iterations: 5,
+  vus: 100,
+  iterations: 100,
 };
 
-export function setup() {}
+export function setup() { }
 
 export default function (data) {
-  const response = http.get(endPoints.FIXED_DELAY_SYNC_API);
+  const response = http.get(endPoints.APP_INFO);
   const status = checkAndVerifyStatus(response, 200);
 }
